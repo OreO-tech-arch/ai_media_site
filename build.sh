f49968bc1f02d844d741f47a5c3f2d7578589e51
OreO <miyamori.reo@gmail.com>
2025-12-06 16:46:56 +0900

    add auto superuser creation

--- build.sh
+++ build.sh
@@ -5,4 +5,14 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
 python manage.py migrate
+
+# ▼ ここを追加！「もし管理者がいなければ、自動で作ってね」という命令です
+python manage.py createsuperuser --noinput || true
